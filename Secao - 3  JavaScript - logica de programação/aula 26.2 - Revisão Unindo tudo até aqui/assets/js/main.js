f3f013779a25f01ca68b890e5e39076a677ec220
carlosmartins1985 <carlostenzel@gmail.com>
2025-04-13 17:49:13 -0300

    Revisão

--- Secao - 3  JavaScript - logica de programação/aula 26.2 - Revisão Unindo tudo até aqui/assets/js/main.js
+++ Secao - 3  JavaScript - logica de programação/aula 26.2 - Revisão Unindo tudo até aqui/assets/js/main.js
@@ -1,163 +1,86 @@
-// Apesar de ser uma boa prática de programação, não usaremos uma função escopo
-/**
-        Passos a serem seguidos para criação do programa
-    
-    * Capturar evento de submit do formulário;
-    // Criamos o formulário no html, bem básico para esse exercício.
-
-    <body>
-    <section class="container">
-        <h1>Calcule seu IMC</h1>
-
-        <form action="" id="form" method="post">
-            <label for="peso">Peso</label>
-            <!--Podemos usar ou não o name-->
-            <input type="text" id="peso" name="peso">
-
-            <label for="altura">Altura</label>
-            <input type="text" id="altura" name="altura">
-
-            <button type="submit">Calcular</button>
-        </form>
-    </section>
-
-    <script src="./assets/js/main.js"></script>
-    </body>
-
-    * Usamos id para selecionar o formulário;
-    * Para parar o envio do formulário, usamos a função addEventListener(), passando como parâmetros, primeiro 
-    qual evento devemos assistir, escutar, que no caso é o submit.
-    * Após isso podemos criar uma função separada, ou adiconar uma função anônima para executar a tarefa.
-    * Como parâmetro dessa função podemos ter, evento, event ou simplesmente e, pois trata-se de e de event.
-     
-    form.addEventListener('submit', function(e) {
-
-    })
-
-    * Adicionamos um log para saber se foi realemente enviado.
-
-    * Agora iremos ao longo do código adicionando funçóes que executam pequenas tarefas, o ideal é termos 
-    uma função para cada tarefa.
-
-    * Adicionamos a div resultado no html.
-
-    * Criamos a função que irá receber o resultado, com o parâmetro msg;
-
-    * Como teste podemos enviar a msg através do parâmetro da função.
-
-    * Chamamos a função setResultado, dentro da função do form, passando um 'Olá mundo' como parâmetro
-
-    const form = document.querySelector("#formulario");
-
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        setResultado('Olá mundo!!');
-        
-    })
-
-    function setResultado(msg) {
-        const resultado = document.querySelector('#resultado');
-
-        resultado.innerHTML = msg;
-    }
-
-    A única coisa que essa função faz é adicionar o html.
-
-    * Faremos diferente, a cada vez que chamarmos o html, iremos zerar o conteúdo
-
-    function setResultado(msg) {
-    const resultado = document.querySelector('#resultado');
-
-    resultado.innerHTML = ''; // Zerando o conteúdo
-    }
-
-    * Usamos uma função para criar um parágrafo;
-
-    function setResultado(msg) {
-        const resultado = document.querySelector('#resultado');
-
-        // Zerar o conteúdo, deixando uma div em branco
-        resultado.innerHTML = '';
-
-        // Aqui criamos uma variável que irá criar um parágrafo
-        const p = document.createElement('p');
-
-        // Aqui conseguimos adicionar uma classe ao parágrafo através do javaScript
-        // Podemos fazer isso em qualquer elemento.
-        p.classList.add('paragrafo-resultado'); 
-
-        p.innerHTML = "Qualquer coisa!!"
-
-        // Aqui adicionamos a variável que contém o parágrafo a div resultado, usando o appendChild().
-        resultado.appendChild(p);
-    }
-
-    * Assim podemos através do javaScript modificar o css do parágrafo.
-
-    * Criamos essa classe no css;
-
-    * Criamos a função criaP();
-
-        // Aqui criamos uma variável que irá criar um parágrafo
-        const p = document.createElement('p');
-        // Aqui conseguimos adicionar uma classe ao parágrafo através do javaScript
-        // Podemos fazer isso em qualquer elemento.
-        p.classList.add('paragrafo-resultado');
-        p.innerHTML = "Qualquer coisa!!"
-        // Aqui adicionamos a variável que contém o parágrafo a div resultado, usando o appendChild().
-        resultado.appendChild(p);
-
-    * Como não queremos que uma função dependa da outra, no caso setResultado e criaP, não colocaremos
-    o innetHTML dentro da função criaP.
-
-    * Retiramos todas as tarefas da função criaP, deixando apenas para restornar o parágrafo.
-
-    function criaP() {
-    const p = document.createElement('p'); // Cria o elemento p
-    return p; // Retorna o elemento p.
-    }
-    
-    * Dentro da função setResultado criamos uma variável que irá receber a função criaP.
-
-    * Capturamos o conteúdo dos inputs.
-    * Como sabemos que o evento está vindo do form, podemos tanto utilizar form quanto e.target, ou seja,
-    de quem está vindo esse evento.
-    * O target pega o elemento que está fazendo o evento, exatamente o elemento, ou seja, de onde estamos 
-    selecionando o elemento que precisamos, no caso de dentro do form, pois os inputs estão dentro do form.
-
- */
-
+// Aqui selecionamos o form que vem do html
 const form = document.querySelector("#formulario");
 
-form.addEventListener('submit', function(e) {
-    // (e) Vem de event.
-    e.preventDefault();
+// Função que evita que o formulário seja enviado
+form.addEventListener("submit", function(e) {
     
-    
-    const inputPeso = e.target.querySelector('#peso');
-    const inputAltura = e.target.querySelector('#altura');
+  // (e) Vem de event.
+  e.preventDefault();
 
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
 
-    console.log(peso, altura);
-    
-    
-    
-})
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
+  if (!peso) {
+    setResultado("Peso inválido.", false);
+    return;
+  }
 
-function criaP() {
-    const p = document.createElement('p');
-    return p;
+  if (!altura) {
+    setResultado("Altura invalida", false);
+    return;
+  }
+
+//   Chamada das funções, onde teremos o calculo e exibição do IMC.
+  const imc = getIMC(peso, altura);
+  const nivelIMC = getNivelIMC(imc);
+  const msg = `O seu imc é ${imc} (${nivelIMC}).`;
+
+//   Por último temos a função que irá exibir o resultado.
+  setResultado(msg, true);
+});
+
+// Funçã para calcular o IMC.
+function getIMC(peso, altura) {
+  const imc = peso / (altura * altura);
+  return imc.toFixed(2);
 }
 
-function setResultado(msg) {
-    const resultado = document.querySelector('#resultado');
+// Função que recebe o IMC e retorna o nível.
+function getNivelIMC(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
 
-    // Zerar o conteúdo, deixando uma div em branco
-    resultado.innerHTML = '';
+  // Aqui invertemos a lógica, e checamos de tras para frente, pois a medida que a função encontrar return, não será mais executada
+  // Uma tática que não é muito recomendado, podemos por tudo em uma linha só.
+  if (imc >= 39.9) return nivel[5]; // Se o valor for maior que 39.9, já retorna o nível.
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
+}
 
-    const p = criaP();
+// Função que cria um parágrafo.
+function criaP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+// Função que exibe o resultado do IMC.
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+
+  // Zerar o conteúdo, deixando uma div em branco
+  resultado.innerHTML = "";
+
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add("paragrafo-resultado");
+  } else {
+    p.classList.add("bad");
+  }
+
+  p.innerHTML = msg;
+  resultado.appendChild(p);
 }
 
